1d294081854982b8200d819e2df2dc4692779e58
f1r1o1n1t1 <zhouhang09@yeah.net>
2017-11-29 22:01:47 +0800

    add a fuck

---
+++ build/CMakeFiles/Poission.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Poission.dir/poission.cpp.o"
+  "Poission.pdb"
+  "Poission"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Poission.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
